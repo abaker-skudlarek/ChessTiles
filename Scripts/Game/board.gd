74e328e91ff259e27f5249567c005886d78f38b2
abaker-skudlarek <abakerskudlarek@gmail.com>
2024-03-26 23:07:25 -0700

    finished $126, closing branch

--- Scripts/Game/board.gd
+++ Scripts/Game/board.gd
@@ -458,6 +458,4 @@ func _on_enemy_move_overlay_clicked(overlay_pixel_location: Vector2) -> void:
 # -- Public Functions -- #
 # ------------------------------------------------------------------------------------------------ #
 
-pass
-
 
