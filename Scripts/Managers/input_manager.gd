8a9c67326ae390e2690587c6e760d75755d952f9
abaker-skudlarek <abakerskudlarek@gmail.com>
2024-05-16 21:58:02 -0700

    fixing diagonal movement, moving while already moving, overlays spawning when no chess moves are allowed. $116, $12u

--- Scripts/Managers/input_manager.gd
+++ Scripts/Managers/input_manager.gd
@@ -11,17 +11,9 @@ var _swipe_divergence_threshold: int = 10  # The amount of pixels that the swipe
 										   # or vertical swipe. If the swipe diverges more than this amount of pixels, it 
 										   # will not be detected. Prevents diagonal swipes
 var _is_swiping: bool = false
-var _input_allowed: bool = true
-
 
 # ------------------------------------------------------------------------------------------------ #
 # -- Private Functions -- #
-# ------------------------------------------------------------------------------------------------ #
-
-func _ready() -> void:
-	SignalBus.connect("slide_move_finished", _on_slide_move_finished)
-	SignalBus.connect("chess_move_finished", _on_chess_move_finished)
-
 # ------------------------------------------------------------------------------------------------ #
 
 func _process(_delta: float) -> void:
@@ -30,37 +22,20 @@ func _process(_delta: float) -> void:
 # ------------------------------------------------------------------------------------------------ #
 
 func _input(event: InputEvent) -> void:	
-	if !_input_allowed:
-		print("moving is not allowed")
-		return
-
 	# If we aren't waiting for user input, return and do nothing
 	if GameManager.get_current_game_state() != GameManager.GameState.WAITING_USER_INPUT:
 		return
-	
-	# TODO: It's possible to do two quick movements really fast, would like to make sure that isn't allowed
-	#		Need some sort of wait, or maybe when we have animations we can make it so you can't move
-	# 		until the animation is done
-	# TODO: Need to prevent diagonal moves
-	
+
 	if event.is_action_pressed("slide_move_left"):
-		_input_allowed = false
-		print("_input_allowed: ", _input_allowed)
 		SignalBus.emit_signal("slide_move_left")
 	
 	if event.is_action_pressed("slide_move_right"):
-		_input_allowed = false
-		print("_input_allowed: ", _input_allowed)
 		SignalBus.emit_signal("slide_move_right")
 		
 	if event.is_action_pressed("slide_move_up"):
-		_input_allowed = false
-		print("_input_allowed: ", _input_allowed)
 		SignalBus.emit_signal("slide_move_up")
 		
 	if event.is_action_pressed("slide_move_down"):
-		_input_allowed = false
-		print("_input_allowed: ", _input_allowed)
 		SignalBus.emit_signal("slide_move_down")
 
 # ------------------------------------------------------------------------------------------------ #
@@ -88,17 +63,5 @@ func _swipe_detection() -> void:
 					_is_swiping = false
 	else:
 		_is_swiping = false
-
-# ------------------------------------------------------------------------------------------------ #
-
-func _on_slide_move_finished() -> void:
-	_input_allowed = true
-	print("_input_allowed: ", _input_allowed)
-
-# ------------------------------------------------------------------------------------------------ #
-
-func _on_chess_move_finished() -> void:
-	_input_allowed = true
-	print("_input_allowed: ", _input_allowed)
 
 # ------------------------------------------------------------------------------------------------ #
